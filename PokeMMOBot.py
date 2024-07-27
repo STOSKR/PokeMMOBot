6ee000212d99aca9973a22b6f516210fc4030710
Shiyi Cheng <SCHENG1@upv.edu.es>
2024-07-28 01:47:53 +0200

    Primer commit

--- PokeMMOBot.py
+++ PokeMMOBot.py
@@ -1,1 +1,14 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from UIFunc import UIFunc
+
+def main():
+    app = QApplication(sys.argv)
+    ui_func = UIFunc()
+    ui_func.setFixedSize(652, 476)  # Tamaño ajustado según el archivo UIView.ui
+    ui_func.show()
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
 
